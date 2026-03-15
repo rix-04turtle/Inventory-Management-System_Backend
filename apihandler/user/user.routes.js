57a04ee1ff76321140493e7f155984ee9aba24ea
rix-04turtle <rishita.k004@gmail.com>
2026-03-15 18:13:14 +0530

    feat: add user login functionality with JWT authentication and OpenAPI documentation

--- apihandler/user/user.routes.js
+++ apihandler/user/user.routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import userSignUp from './userSignUp.js'
+import userLogin from './userLogin.js'
 
 
 const userRoutes = express()
 
 userRoutes.post('/signup', userSignUp)
+
+userRoutes.post('/login', userLogin)
 
 
 // BASE URL : /user
---
+++ apihandler/user/userLogin.js
@@ -0,0 +1,54 @@
+import Users from "../../models/Users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+async function userLogin(req, res) {
+  try {
+    let body = req.body;
+    console.log(body);
+
+    let email = body.email;
+    let password = body.password;
+    if (typeof password !== "string") {
+      return res.json({ error: "Type of password must be String" });
+    }
+
+    console.log(email, password);
+
+    const existingUser = await Users.findOne({
+      email: email,
+    });
+
+    console.log(existingUser);
+
+    if (!existingUser) {
+      return res.status(400).send("Incorrect email or password.");
+    }
+
+    console.log(typeof password);
+    const isMatch = await bcrypt.compare(password, existingUser.passwordHash);
+
+    if (!isMatch) {
+      return res.status(400).send("Incorrect email or password.");
+    }
+
+    const payload = { id: existingUser._id, role: existingUser.role, email: existingUser.email}
+
+    const token = jwt.sign(
+        payload,
+        process.env.JWT_SECRET_KEY,
+        {expiresIn: "30d"}
+    );
+
+    return res.json({ status: "success", token});
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      status: "failed",
+      message: "Internal Server Error",
+      err,
+    });
+  }
+}
+
+export default userLogin;
